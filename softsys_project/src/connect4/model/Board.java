109c5e13c1fb9ac1eee1e8dd3e992658feee0969
wildd0g <mpeppelman2008@hotmail.com>
2017-01-18 14:43:13 +0100

    board WIP

--- softsys_project/src/connect4/model/Board.java
+++ softsys_project/src/connect4/model/Board.java
@@ -7,326 +7,348 @@ package connect4.model;
  * @version $Revision: 1.4 $
  */
 public class Board {
-    public static final int DIM = 3;
-    private static final String[] NUMBERING = {" 0 | 1 | 2 ", "---+---+---",
+	public static int DIMROW = 4;
+	public static int DIMCOL = 4;
+	public static int DIMLVL = 4;
+	/*private static final String[] NUMBERING = {" 0 | 1 | 2 ", "---+---+---",
         " 3 | 4 | 5 ", "---+---+---", " 6 | 7 | 8 "};
     private static final String LINE = NUMBERING[1];
-    private static final String DELIM = "     ";
+    private static final String DELIM = "     ";*/
 
-    /**
-     * The DIM by DIM fields of the Tic Tac Toe student. See NUMBERING for the
-     * coding of the fields.
-     */
-    //@ private invariant fields.length == DIM*DIM;
-    /*@ invariant (\forall int i; 0 <= i & i < DIM*DIM;
+	/**
+	 * The DIM by DIM fields of the Tic Tac Toe student. See NUMBERING for the
+	 * coding of the fields.
+	 */
+	//@ private invariant fields.length == DIM*DIM;
+	/*@ invariant (\forall int i; 0 <= i & i < DIM*DIM;
         getField(i) == Mark.EMPTY || getField(i) == Mark.XX || getField(i) == Mark.OO); */
-    
-    private Mark[] fields = new Mark[9];
 
-    // -- Constructors -----------------------------------------------
+	private Mark[] fields = new Mark[DIMROW * DIMCOL * DIMLVL];
 
-    /**
-     * Creates an empty student.
-     */
-    //@ ensures (\forall int i; 0 <= i & i < DIM * DIM; this.getField(i) == Mark.EMPTY);
-    public Board() {
-    	reset();
-    }
+	// -- Constructors -----------------------------------------------
 
-    /**
-     * Creates a deep copy of this field.
-     */
-    /*@ ensures \result != this;
-        ensures (\forall int i; 0 <= i & i < DIM * DIM;
+	/**
+	 * Creates an empty student.
+	 */
+	//@ ensures (\forall int i; 0 <= i & i < DIMROW * DIMCOL * DIMLVL; this.getField(i) == Mark.EMPTY);
+	public Board() {
+		reset();
+	}
+
+	public Board(int row, int col, int lvl) {
+		fields = new Mark[DIMROW * DIMCOL * DIMLVL];
+		reset();
+	}
+	/**
+	 * Creates a deep copy of this field.
+	 */
+	/*@ ensures \result != this;
+        ensures (\forall int i; 0 <= i & i < DIMROW * DIMCOL * DIMLVL;
                                 \result.getField(i) == this.getField(i));
       @*/
-    public Board deepCopy() {
-    	Board copy = new Board();
-    	for (int i = 0; i < DIM * DIM; i++){
-        	copy.fields[i] = this.fields[i];
-        	}
-        return copy;
-    }
+	public Board deepCopy() {
+		Board copy = new Board(DIMROW, DIMCOL, DIMLVL);
+		for (int i = 0; i < DIMROW * DIMCOL * DIMLVL; i++) {
+			copy.fields[i] = this.fields[i];
+		}
+		return copy;
+	}
 
-    /**
-     * Calculates the index in the linear array of fields from a (row, col)
-     * pair.
-     * @return the index belonging to the (row,col)-field
-     */
-    //@ requires 0 <= row & row < DIM;
-    //@ requires 0 <= col & col < DIM;
-    /*@pure*/
-    public int index(int row, int col) {
-    	int i = (DIM * row) + col;
-        return i;
-    }
+	/**
+	 * Calculates the index in the linear array of fields from a (row, col)
+	 * pair.
+	 * @return the index belonging to the (row,col)-field
+	 */
+	//@ requires 0 <= row & row < DIMROW;
+	//@ requires 0 <= col & col < DIMCOL;
+	//@ requires 0 <= lvl & lvl < DIMLVL;
+	/*@pure*/
+	public int index(int row, int col, int lvl) {
+		int i = (DIMCOL * (DIMROW * lvl + row)) + col;
+		return i;
+	}
 
-    /**
-     * Returns true if ix is a valid index of a field on the student.
-     * @return true if 0 <= index < DIM*DIM
-     */
-    //@ ensures \result == (0 <= index && index < DIM * DIM);
-    /*@pure*/
-    public boolean isField(int index) {
-        return (0 <= index && index < DIM * DIM);
-    }
+	/**
+	 * Returns true if ix is a valid index of a field on the student.
+	 * @return true if 0 <= index < DIM*DIM
+	 */
+	//@ ensures \result == (0 <= index && index < DIMROW * DIMCOL * DIMLVL);
+	/*@pure*/
+	public boolean isField(int index) {
+		return 0 <= index && index < DIMROW * DIMCOL * DIMLVL;
+	}
 
-    /**
-     * Returns true of the (row,col) pair refers to a valid field on the student.
-     *
-     * @return true if 0 <= row < DIM && 0 <= col < DIM
-     */
-    //@ ensures \result == (0 <= row && row < DIM && 0 <= col && col < DIM);
-    /*@pure*/
-    public boolean isField(int row, int col) {
-        return (isField(index(row,col)));
-    }
-    
-    /**
-     * Returns the content of the field i.
-     *
-     * @param i
-     *            the number of the field (see NUMBERING)
-     * @return the mark on the field
-     */
-    //@ requires this.isField(i);
-    //@ ensures \result == Mark.EMPTY || \result == Mark.XX || \result == Mark.OO;
-    /*@pure*/
-    public Mark getField(int i) {
-    	Mark result = fields[i];
-        return result;
-    }
+	/**
+	 * Returns true of the (row,col) pair refers to a valid field on the student.
+	 *
+	 * @return true if 0 <= row < DIM && 0 <= col < DIM
+	 */
+	//@ ensures \result == (0 <= row && row < DIM && 0 <= col && col < DIM);
+	/*@pure*/
+	public boolean isField(int row, int col, int lvl) {
+		return isField(index(row, col, lvl));
+	}
 
-    /**
-     * Returns the content of the field referred to by the (row,col) pair.
-     *
-     * @param row
-     *            the row of the field
-     * @param col
-     *            the column of the field
-     * @return the mark on the field
-     */
-    //@ requires this.isField(row,col);
-    //@ ensures \result == Mark.EMPTY || \result == Mark.XX || \result == Mark.OO;
-    /*@pure*/
-    public Mark getField(int row, int col) {
-        return getField(index(row, col));
-    }
+	/**
+	 * Returns the content of the field i.
+	 *
+	 * @param i
+	 *            the number of the field (see NUMBERING)
+	 * @return the mark on the field
+	 */
+	//@ requires this.isField(i);
+	//@ ensures \result == Mark.EMPTY || \result == Mark.XX || \result == Mark.OO;
+	/*@pure*/
+	public Mark getField(int i) {
+		Mark result = fields[i];
+		return result;
+	}
 
-    /**
-     * Returns true if the field i is empty.
-     *
-     * @param i
-     *            the index of the field (see NUMBERING)
-     * @return true if the field is empty
-     */
-    //@ requires this.isField(i);
-    //@ ensures \result == (this.getField(i) == Mark.EMPTY);
-    /*@pure*/
-    public boolean isEmptyField(int i) {
-        return (getField(i) == Mark.EMPTY);
-    }
+	/**
+	 * Returns the content of the field referred to by the (row,col) pair.
+	 *
+	 * @param row
+	 *            the row of the field
+	 * @param col
+	 *            the column of the field
+	 * @return the mark on the field
+	 */
+	//@ requires this.isField(row,col);
+	//@ ensures \result == Mark.EMPTY || \result == Mark.XX || \result == Mark.OO;
+	/*@pure*/
+	public Mark getField(int row, int col, int lvl) {
+		return getField(index(row, col, lvl));
+	}
 
-    /**
-     * Returns true if the field referred to by the (row,col) pair it empty.
-     *
-     * @param row
-     *            the row of the field
-     * @param col
-     *            the column of the field
-     * @return true if the field is empty
-     */
-    //@ requires this.isField(row,col);
-    //@ ensures \result == (this.getField(row,col) == Mark.EMPTY);
-    /*@pure*/
-    public boolean isEmptyField(int row, int col) {
-        return isEmptyField(index(row,col));
-    }
+	/**
+	 * Returns true if the field i is empty.
+	 *
+	 * @param i
+	 *            the index of the field (see NUMBERING)
+	 * @return true if the field is empty
+	 */
+	//@ requires this.isField(i);
+	//@ ensures \result == (this.getField(i) == Mark.EMPTY);
+	/*@pure*/
+	public boolean isEmptyField(int i) {
+		return getField(i) == Mark.EMPTY;
+	}
 
-    /**
-     * Tests if the whole student is full.
-     *
-     * @return true if all fields are occupied
-     */
-    //@ ensures \result == (\forall int i; i <= 0 & i < DIM * DIM; this.getField(i) != Mark.EMPTY);
-    /*@pure*/
-    public boolean isFull() {
-    	boolean result = true;
-    	for (int i = 0; i < DIM*DIM; i++){
-        	result = result && !isEmptyField(i);
-        	}
-        return result;
-    }
+	/**
+	 * Returns true if the field referred to by the (row,col) pair it empty.
+	 *
+	 * @param row
+	 *            the row of the field
+	 * @param col
+	 *            the column of the field
+	 * @return true if the field is empty
+	 */
+	//@ requires this.isField(row,col);
+	//@ ensures \result == (this.getField(row,col) == Mark.EMPTY);
+	/*@pure*/
+	public boolean isEmptyField(int row, int col, int lvl) {
+		return isEmptyField(index(row, col, lvl));
+	}
 
-    /**
-     * Returns true if the game is over. The game is over when there is a winner
-     * or the whole student is full.
-     *
-     * @return true if the game is over
-     */
-    //@ ensures \result == this.isFull() || this.hasWinner();
-    /*@pure*/
-    public boolean gameOver() {
-        return (isFull() || isWinner(Mark.XX) || isWinner(Mark.OO));
-    }
+	/**
+	 * Tests if the whole student is full.
+	 *
+	 * @return true if all fields are occupied
+	 */
+	//@ ensures \result == (\forall int i; i <= 0 & i < DIMROW * DIMCOL * DIMLVL; this.getField(i) != Mark.EMPTY);
+	/*@pure*/
+	public boolean isFull() {
+		boolean result = true;
+		for (int i = 0; i < DIMROW * DIMCOL * DIMLVL; i++) {
+			if (!isEmptyField(i)) {
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
 
-    /**
-     * Checks whether there is a row which is full and only contains the mark
-     * m.
-     *
-     * @param m
-     *            the mark of interest
-     * @return true if there is a row controlled by m
-     */
-    /*@ pure */
-    public boolean hasRow(Mark m) {
-    	boolean result = false;
-    	for (int i = 0; i < DIM; i++){
-    		boolean resultI = true;
-    		for (int j = 0; j < DIM; j++){
-    			resultI = (resultI && (getField(index(i,j)) == m));
-        	}
-    		result = (result || resultI);
-    	}
-        return result;
-    }
+	/**
+	 * Returns true if the game is over. The game is over when there is a winner
+	 * or the whole student is full.
+	 *
+	 * @return true if the game is over
+	 */
+	//@ ensures \result == this.isFull() || this.hasWinner();
+	/*@pure*/
+	public boolean gameOver() {
+		return isFull() || hasWinner();
+	}
 
-    /**
-     * Checks whether there is a column which is full and only contains the mark
-     * m.
-     *
-     * @param m
-     *            the mark of interest
-     * @return true if there is a column controlled by m
-     */
-    /*@ pure */
-    public boolean hasColumn(Mark m) {
-    	boolean result = false;
-    	for (int i = 0; i < DIM; i++){
-    		boolean resultI = true;
-    		for (int j = 0; j < DIM; j++){
-    			resultI = (resultI && (getField(index(j,i)) == m));
-        	}
-    		result = (result || resultI);
-    	}
-        return result;
-    }
+	/**
+	 * Checks whether there is a row which is full and only contains the mark
+	 * m.
+	 *
+	 * @param m
+	 *            the mark of interest
+	 * @return true if there is a row controlled by m
+	 */
+	/*@ pure */
+	public boolean hasRow(Mark m) {
+		boolean result = false;
+		for (int i = 0; i < DIMCOL; i++) {
+			for (int j = 0; j < DIMLVL; j++) { 
+				for (int k = 3; k < DIMROW; k++) {
+					result = getField(index(i, j, k - 3)) == m &&
+					getField(index(i, j, k - 2)) == m &&
+					getField(index(i, j, k - 1)) == m &&
+					getField(index(i, j, k)) == m;
+					if (result) {
+						break;
+					}
+				}
+				if (result) {
+					break;
+				}
+			}
+			if (result) {
+				break;
+			}
+		}
+		return result;
+	}
 
-    /**
-     * Checks whether there is a diagonal which is full and only contains the
-     * mark m.
-     *
-     * @param m
-     *            the mark of interest
-     * @return true if there is a diagonal controlled by m
-     */
-    /*@ pure */
-    public boolean hasDiagonal(Mark m) {
-    	boolean result = false;
-    	boolean resultI = true;
-    	for (int i = 0; i < DIM; i++){
-    		resultI = (resultI && (getField(index(i,i)) == m));
-    	}
-    	result = (result || resultI);
-    	resultI = true;
-    	for (int i = 0; i < DIM; i++){
-    		resultI = (resultI && (getField(index(i,DIM-i-1)) == m));
-    	}
-    	result = (result || resultI);
-    	return result;
-    }
+	/**
+	 * Checks whether there is a column which is full and only contains the mark
+	 * m.
+	 *
+	 * @param m
+	 *            the mark of interest
+	 * @return true if there is a column controlled by m
+	 */
+	/*@ pure */
+	public boolean hasColumn(Mark m) {
+		boolean result = false;
+		for (int i = 0; i < DIM; i++){
+			boolean resultI = true;
+			for (int j = 0; j < DIM; j++){
+				resultI = (resultI && (getField(index(j,i)) == m));
+			}
+			result = (result || resultI);
+		}
+		return result;
+	}
 
-    /**
-     * Checks if the mark m has won. A mark wins if it controls at
-     * least one row, column or diagonal.
-     *
-     * @param m
-     *            the mark of interest
-     * @return true if the mark has won
-     */
-    //@requires m == Mark.XX | m == Mark.OO;
-    //@ ensures \result == this.hasRow(m) || this.hasColumn(m) | this.hasDiagonal(m);
-    /*@ pure */
-    public boolean isWinner(Mark m) {
-        return (hasRow(m) || hasColumn(m) || hasDiagonal(m));
-    }
+	/**
+	 * Checks whether there is a diagonal which is full and only contains the
+	 * mark m.
+	 *
+	 * @param m
+	 *            the mark of interest
+	 * @return true if there is a diagonal controlled by m
+	 */
+	/*@ pure */
+	public boolean hasDiagonal(Mark m) {
+		boolean result = false;
+		boolean resultI = true;
+		for (int i = 0; i < DIM; i++){
+			resultI = (resultI && (getField(index(i,i)) == m));
+		}
+		result = (result || resultI);
+		resultI = true;
+		for (int i = 0; i < DIM; i++){
+			resultI = (resultI && (getField(index(i,DIM-i-1)) == m));
+		}
+		result = (result || resultI);
+		return result;
+	}
 
-    /**
-     * Returns true if the game has a winner. This is the case when one of the
-     * marks controls at least one row, column or diagonal.
-     *
-     * @return true if the student has a winner.
-     */
-    //@ ensures \result == isWinner(Mark.XX) | \result == isWinner(Mark.OO);
-    /*@pure*/
-    public boolean hasWinner() {
-        return (isWinner(Mark.XX) || isWinner(Mark.OO));
-    }
+	/**
+	 * Checks if the mark m has won. A mark wins if it controls at
+	 * least one row, column or diagonal.
+	 *
+	 * @param m
+	 *            the mark of interest
+	 * @return true if the mark has won
+	 */
+	//@requires m == Mark.XX | m == Mark.OO;
+	//@ ensures \result == this.hasRow(m) || this.hasColumn(m) | this.hasDiagonal(m);
+	/*@ pure */
+	public boolean isWinner(Mark m) {
+		return (hasRow(m) || hasColumn(m) || hasDiagonal(m));
+	}
 
-    /**
-     * Returns a String representation of this student. In addition to the current
-     * situation, the String also shows the numbering of the fields.
-     *
-     * @return the game situation as String
-     */
-    public String toString() {
-        String s = "";
-        for (int i = 0; i < DIM; i++) {
-            String row = "";
-            for (int j = 0; j < DIM; j++) {
-                row = row + " " + getField(i, j).toString() + " ";
-                if (j < DIM - 1) {
-                    row = row + "|";
-                }
-            }
-            s = s + row + DELIM + NUMBERING[i * 2];
-            if (i < DIM - 1) {
-                s = s + "\n" + LINE + DELIM + NUMBERING[i * 2 + 1] + "\n";
-            }
-        }
-        return s;
-    }
+	/**
+	 * Returns true if the game has a winner. This is the case when one of the
+	 * marks controls at least one row, column or diagonal.
+	 *
+	 * @return true if the student has a winner.
+	 */
+	//@ ensures \result == isWinner(Mark.XX) | \result == isWinner(Mark.OO);
+	/*@pure*/
+	public boolean hasWinner() {
+		return (isWinner(Mark.XX) || isWinner(Mark.OO));
+	}
 
-    /**
-     * Empties all fields of this student (i.e., let them refer to the value
-     * Mark.EMPTY).
-     */
-    /*@ ensures (\forall int i; 0 <= i & i < DIM * DIM;
+	/**
+	 * Returns a String representation of this student. In addition to the current
+	 * situation, the String also shows the numbering of the fields.
+	 *
+	 * @return the game situation as String
+	 */
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < DIM; i++) {
+			String row = "";
+			for (int j = 0; j < DIM; j++) {
+				row = row + " " + getField(i, j).toString() + " ";
+				if (j < DIM - 1) {
+					row = row + "|";
+				}
+			}
+			s = s + row + DELIM + NUMBERING[i * 2];
+			if (i < DIM - 1) {
+				s = s + "\n" + LINE + DELIM + NUMBERING[i * 2 + 1] + "\n";
+			}
+		}
+		return s;
+	}
+
+	/**
+	 * Empties all fields of this student (i.e., let them refer to the value
+	 * Mark.EMPTY).
+	 */
+	/*@ ensures (\forall int i; 0 <= i & i < DIMROW * DIMCOL * DIMLVL;
                                 this.getField(i) == Mark.EMPTY); @*/
-    public void reset() {
-    	for (int i = 0; i < DIM*DIM; i++){
-        	setField(i, Mark.EMPTY);
-        	}
-    }
+	public void reset() {
+		for (int i = 0; i < DIMROW*DIMCOL*DIMLVL; i++){
+			setField(i, Mark.EMPTY);
+		}
+	}
 
-    /**
-     * Sets the content of field i to the mark m.
-     *
-     * @param i
-     *            the field number (see NUMBERING)
-     * @param m
-     *            the mark to be placed
-     */
-    //@ requires this.isField(i);
-    //@ ensures this.getField(i) == m;
-    public void setField(int i, Mark m) {
-    	fields[i] = m;
-    }
+	/**
+	 * Sets the content of field i to the mark m.
+	 *
+	 * @param i
+	 *            the field number (see NUMBERING)
+	 * @param m
+	 *            the mark to be placed
+	 */
+	//@ requires this.isField(i);
+	//@ ensures this.getField(i) == m;
+	public void setField(int i, Mark m) {
+		fields[i] = m;
+	}
 
-    /**
-     * Sets the content of the field represented by the (row,col) pair to the
-     * mark m.
-     *
-     * @param row
-     *            the field's row
-     * @param col
-     *            the field's column
-     * @param m
-     *            the mark to be placed
-     */
-    //@ requires this.isField(row,col);
-    //@ ensures this.getField(row,col) == m;
-    public void setField(int row, int col, Mark m) {
-    	setField(index(row, col), m);
-    }
+	/**
+	 * Sets the content of the field represented by the (row,col) pair to the
+	 * mark m.
+	 *
+	 * @param row
+	 *            the field's row
+	 * @param col
+	 *            the field's column
+	 * @param m
+	 *            the mark to be placed
+	 */
+	//@ requires this.isField(row,col);
+	//@ ensures this.getField(row,col) == m;
+	public void setField(int row, int col, Mark m) {
+		setField(index(row, col), m);
+	}
 }

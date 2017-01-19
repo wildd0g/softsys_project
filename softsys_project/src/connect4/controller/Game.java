41b17318d3639539b8ba009130fef76a5dbe5634
Merel <Merel@LAPTOP-4OEJA8EL>
2017-01-19 11:34:12 +0100

    minor changes to Class Game

--- softsys_project/src/connect4/controller/Game.java
+++ softsys_project/src/connect4/controller/Game.java
@@ -2,9 +2,28 @@ package connect4.controller;
 
 public class Game {
 
-	//method
+	//method to send a message from Server to Client
 	public void sendString(String message){
 		
 	}
+
+	//method to broadcast a message from Server to Clients in one game
+	public void sendAllString(String message){
+
+	}
 	
+	
+
+	//method to receive message from Client as Server
+	public void receiveString(){
+
+	}
+	
+	//method that utilises protocol to translate received message into action
+	public void processString(){
+		
+	}
+
+
+
 }

9fed315f75ea40a132886343eb71e6ff96729526
Merel <Merel@LAPTOP-4OEJA8EL>
2017-01-19 15:43:44 +0100

    15:43 timeViolation Implemented

--- softsys_project/src/controller/Game.java
+++ softsys_project/src/controller/Game.java
@@ -8,12 +8,13 @@ public class Game implements Runnable{
 	public int currentPlaying = 0;
 	public Player[] players;
 	public int gameID;
-	public Calendar timeout;
+	public long timeout;
 	
 	public Game(int id, int playerNum, int dimX, int dimY, int dimZ, int winLength){
 		players = new Player[playerNum];
 		this.gameID = id;
-		timeout = Calendar.getInstance();
+		timeout = System.currentTimeMillis();
+		
 	}
 	
 	//method that adds a player to this room
@@ -26,21 +27,41 @@ public class Game implements Runnable{
 	//method that removes a player from the room
 	//action for command leaveRoom
 	public void removePlayer(Player gonePlayer){
+		
 		//for loop browses through the list of players logged in this room 
 		//(no need for those who added later because they can't remove themselves until after they've been added)
 		for(int i = 0; i <= currentPlaying; i++){
+			
 			//if the specified player is found remove it and set currentPlaying to one less
 			if(players[i].equals(gonePlayer)){
+			
 				players[i] = null;
 				currentPlaying = currentPlaying - 1;
+				
 				break;
+				
 			} 
 		}
 	}
 	
+	public Boolean timeViolation(){
+		//returnable boolean
+		boolean violation = false;
+		
+		//takes present time
+		long now = System.currentTimeMillis();
+		
+		//checks if 2 minutes have passed since last player was given turn
+		if ((now - timeout) > 120000){
+			violation  = true;
+		} 
+		
+		return violation;
+	}
+	
 	//run method to allow communication with the clients
 	public void run(){
-		r
+		
 	}
 	
 	

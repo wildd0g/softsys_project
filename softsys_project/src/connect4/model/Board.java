bf05e908f3a3c8b4c1820e9801cb539fe270fdb8
wildd0g <mpeppelman2008@hotmail.com>
2017-01-19 14:35:37 +0100

    Board almost done, toString left.

--- softsys_project/src/connect4/model/Board.java
+++ softsys_project/src/connect4/model/Board.java
@@ -1,5 +1,7 @@
 package connect4.model;
 
+import java.lang.Math;
+
 /**
  * Game student for the Tic Tac Toe game. Module 2 lab assignment.
  *
@@ -10,6 +12,7 @@ public class Board {
 	public static int DIMROW = 4;
 	public static int DIMCOL = 4;
 	public static int DIMLVL = 4;
+	public static int WIN_LENGTH = 4;
 	/*private static final String[] NUMBERING = {" 0 | 1 | 2 ", "---+---+---",
         " 3 | 4 | 5 ", "---+---+---", " 6 | 7 | 8 "};
     private static final String LINE = NUMBERING[1];
@@ -35,7 +38,11 @@ public class Board {
 		reset();
 	}
 
-	public Board(int row, int col, int lvl) {
+	public Board(int row, int col, int lvl, int win) {
+		DIMROW = row;
+		DIMCOL = col;
+		DIMLVL = lvl;
+		WIN_LENGTH = win;
 		fields = new Mark[DIMROW * DIMCOL * DIMLVL];
 		reset();
 	}
@@ -47,7 +54,7 @@ public class Board {
                                 \result.getField(i) == this.getField(i));
       @*/
 	public Board deepCopy() {
-		Board copy = new Board(DIMROW, DIMCOL, DIMLVL);
+		Board copy = new Board(DIMROW, DIMCOL, DIMLVL, WIN_LENGTH);
 		for (int i = 0; i < DIMROW * DIMCOL * DIMLVL; i++) {
 			copy.fields[i] = this.fields[i];
 		}
@@ -181,61 +188,44 @@ public class Board {
 	}
 
 	/**
-	 * Checks whether there is a row which is full and only contains the mark
+	 * Checks whether there is a line of a given length which only contains the mark
 	 * m.
 	 *
+	 * @param dir[]
+	 * 				the direction to search in, defined by an array of 3 integers,
+	 * 				ranging from -1 to 1. index 0 is row direction,
+	 * 				index 1 is column direction and index 2 is level direction.
+	 *            
 	 * @param m
-	 *            the mark of interest
+	 * 				the mark of interest
+	 * @param length
+	 * 				the length of the line there needs to be checked for
 	 * @return true if there is a row controlled by m
 	 */
+	//Checks in the direction indicated by dir (1 , 0 or -1) for the mark m
+	//during the specified length
 	/*@ pure */
 	//TODO: eventually change to streams, or if () return?
-	public boolean hasRow(Mark m) {
-		boolean result = false;
-		for (int i = 0; i < DIMCOL; i++) {
-			for (int j = 0; j < DIMLVL; j++) { 
-				for (int k = 3; k < DIMROW; k++) {
-					result = getField(index(i, j, k - 3)) == m &&
-					getField(index(i, j, k - 2)) == m &&
-					getField(index(i, j, k - 1)) == m &&
-					getField(index(i, j, k)) == m;
-					if (result) { 
-						break;
-					}
-				}
-				if (result) {
-					break;
-				}
-			}
-			if (result) {
-				break;
-			} 
-		}
-		return result;
-	}
-
-	
-	//Checks in the direction indicated by dir (1 , 0 or -1) for the mark m during the specefied length
 	private boolean checkConnect(int[] dir, Mark m, int length) {
-		if (dir.length != 3 || (dir[0] == 0 && dir[1] == 0 && dir[2] == 0)) {
+		/*if (dir.length != 3 || (dir[0] == 0 && dir[1] == 0 && dir[2] == 0)) {
 			//TODO add exception, this is an error.
-		}
-		//TODO get this right!
-		int rowMin = 0;
-		int rowMax = DIMROW;
-		int calMin = 0;
-		int colMax = DIMCOL;
-		int lvlMin = 0;
-		int lvlMax = DIMLVL;
+		}*/
+		int rowMin = Math.max(0, 0 - dir[0] * length);
+		int rowMax = Math.min(DIMROW, DIMROW - dir[0] * length);
+		int colMin = Math.max(0, 0 - dir[1] * length);
+		int colMax = Math.min(DIMCOL, DIMCOL - dir[1] * length);
+		int lvlMin = Math.max(0, 0 - dir[2] * length);
+		int lvlMax = Math.min(DIMLVL, DIMLVL - dir[2] * length);
 		
 		boolean result = false;
-		for (int i = 0; i < DIMROW; i++) {
-			for (int j = 0; j < DIMCOL; j++) { 
-				for (int k = 0; k < DIMLVL; k++) {
+		for (int i = rowMin; i < rowMax; i++) {
+			for (int j = colMin; j < colMax; j++) { 
+				for (int k = lvlMin; k < lvlMax; k++) {
 					boolean resultL = true;
 					for (int l = 0; l < length; l++) {
 						resultL = getField(index(
-								i + dir[0] * l, j + dir[1] * l, k + dir[2] * l)) == m;
+								i + dir[0] * l, j + dir[1] * l, k + dir[2] * l
+								)) == m;
 						if (!resultL) {
 							break;
 						}
@@ -254,10 +244,24 @@ public class Board {
 			} 
 		}
 		return result;
-		
-		
-		return false;
 	}
+
+	
+	/**
+	 * Checks whether there is a row which is full and only contains the mark
+	 * m.
+	 *
+	 * @param m
+	 *            the mark of interest
+	 * @return true if there is a row controlled by m
+	 */
+	/*@ pure */
+	public boolean hasRow(Mark m) {
+		int[] dir = {1, 0, 0};
+		return checkConnect(dir, m, WIN_LENGTH);
+	}
+
+	
 	
 	/**
 	 * Checks whether there is a column which is full and only contains the mark
@@ -268,29 +272,9 @@ public class Board {
 	 * @return true if there is a column controlled by m
 	 */
 	/*@ pure */
-	//TODO: eventually change to streams, or if () return?
 	public boolean hasColumn(Mark m) {
-		boolean result = false;
-		for (int i = 0; i < DIMCOL; i++) {
-			for (int j = 3; j < DIMLVL; j++) { 
-				for (int k = 0; k < DIMROW; k++) {
-					result = getField(index(i, j - 3, k)) == m &&
-					getField(index(i, j - 2, k)) == m &&
-					getField(index(i, j - 1, k)) == m &&
-					getField(index(i, j, k)) == m;
-					if (result) { 
-						break;
-					}
-				}
-				if (result) {
-					break;
-				}
-			}
-			if (result) {
-				break;
-			} 
-		}
-		return result;
+		int[] dir = {0, 1, 0};
+		return checkConnect(dir, m, WIN_LENGTH);
 	}
 
 	/**
@@ -302,33 +286,13 @@ public class Board {
 	 * @return true if there is a column controlled by m
 	 */
 	/*@ pure */
-	//TODO: eventually change to streams, or if () return?
 	public boolean hasLevels(Mark m) {
-		boolean result = false;
-		for (int i = 3; i < DIMCOL; i++) {
-			for (int j = 0; j < DIMLVL; j++) { 
-				for (int k = 0; k < DIMROW; k++) {
-					result = getField(index(i - 3, j, k)) == m &&
-					getField(index(i - 2, j, k)) == m &&
-					getField(index(i - 1, j, k)) == m &&
-					getField(index(i, j, k)) == m;
-					if (result) { 
-						break;
-					}
-				}
-				if (result) {
-					break;
-				}
-			}
-			if (result) {
-				break;
-			} 
-		}
-		return result;
+		int[] dir = {0, 0, 1};
+		return checkConnect(dir, m, WIN_LENGTH);
 	}
 	
 	/**
-	 * Checks whether there is a diagonal which is full and only contains the
+	 * Checks whether there is a face diagonal which only contains the
 	 * mark m.
 	 *
 	 * @param m
@@ -337,34 +301,65 @@ public class Board {
 	 */
 	/*@ pure */
 	
-	public boolean hasDiagonal(Mark m) {
+	public boolean hasFaceDiagonal(Mark m) {
 		boolean result = false;
-		boolean resultI = true;
-		for (int i = 0; i < DIM; i++){
-			resultI = (resultI && (getField(index(i,i)) == m));
+		int[] dirOnRow1 = {0, 1, 1};
+		int[] dirOnRow2 = {0, 1, -1};
+		int[] dirOnCol1 = {1, 0, 1};
+		int[] dirOnCol2 = {1, 0, -1};
+		int[] dirOnLvl1 = {1, 1, 0};
+		int[] dirOnLvl2 = {1, -1, 0};
+		int[][] directions = {dirOnRow1, dirOnRow2, dirOnCol1, dirOnCol2, dirOnLvl1, dirOnLvl2};
+		for (int i = 0; i < 6; i++) {
+			result = checkConnect(directions[i], m, WIN_LENGTH);	
+			if (result) {
+				break;
+			}
 		}
-		result = (result || resultI);
-		resultI = true;
-		for (int i = 0; i < DIM; i++){
-			resultI = (resultI && (getField(index(i,DIM-i-1)) == m));
+		return result;
+	}
+	
+	/**
+	 * Checks whether there is a space diagonal which only contains the
+	 * mark m.
+	 *
+	 * @param m
+	 *            the mark of interest
+	 * @return true if there is a diagonal controlled by m
+	 */
+	/*@ pure */
+	
+	public boolean hasSpaceDiagonal(Mark m) {
+		boolean result = false;
+		int[] dir1 = {1, 1, 1};
+		int[] dir2 = {1, 1, -1};
+		int[] dir3 = {1, -1, 1};
+		int[] dir4 = {1, -1, -1};
+		int[][] directions = {dir1, dir2, dir3, dir4};
+		for (int i = 0; i < 4; i++) {
+			result = checkConnect(directions[i], m, WIN_LENGTH);	
+			if (result) {
+				break;
+			}
 		}
-		result = (result || resultI);
 		return result;
 	}
 
 	/**
-	 * Checks if the mark m has won. A mark wins if it controls at
-	 * least one row, column or diagonal.
+	 * Checks if the mark m has won. A mark wins if has a line
+	 * along a row, column, across levels, in the direction of any face diagonal
+	 * or in the direction of any space diagonal of the set win length.
 	 *
 	 * @param m
 	 *            the mark of interest
 	 * @return true if the mark has won
 	 */
-	//@requires m == Mark.XX | m == Mark.OO;
+	//@requires m;
 	//@ ensures \result == this.hasRow(m) || this.hasColumn(m) | this.hasDiagonal(m);
 	/*@ pure */
 	public boolean isWinner(Mark m) {
-		return (hasRow(m) || hasColumn(m) || hasDiagonal(m));
+		return hasRow(m) || hasColumn(m) || hasLevels(m)
+				|| hasFaceDiagonal(m) || hasSpaceDiagonal(m);
 	}
 
 	/**
@@ -376,7 +371,7 @@ public class Board {
 	//@ ensures \result == isWinner(Mark.XX) | \result == isWinner(Mark.OO);
 	/*@pure*/
 	public boolean hasWinner() {
-		return (isWinner(Mark.XX) || isWinner(Mark.OO));
+		return isWinner(Mark.XX) || isWinner(Mark.OO);
 	}
 
 	/**
@@ -410,7 +405,7 @@ public class Board {
 	/*@ ensures (\forall int i; 0 <= i & i < DIMROW * DIMCOL * DIMLVL;
                                 this.getField(i) == Mark.EMPTY); @*/
 	public void reset() {
-		for (int i = 0; i < DIMROW*DIMCOL*DIMLVL; i++){
+		for (int i = 0; i < DIMROW * DIMCOL * DIMLVL; i++) {
 			setField(i, Mark.EMPTY);
 		}
 	}
@@ -442,7 +437,7 @@ public class Board {
 	 */
 	//@ requires this.isField(row,col);
 	//@ ensures this.getField(row,col) == m;
-	public void setField(int row, int col, Mark m) {
-		setField(index(row, col), m);
+	public void setField(int row, int col, int lvl, Mark m) {
+		setField(index(row, col, lvl), m);
 	}
 }

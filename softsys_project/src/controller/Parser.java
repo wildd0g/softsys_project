c7e3852a8a24002c969c8300de829f9bfc752fd2
wildd0g <mpeppelman2008@hotmail.com>
2017-01-23 15:48:04 +0100

    15:47 work on parser

--- softsys_project/src/controller/Parser.java
+++ softsys_project/src/controller/Parser.java
@@ -1,10 +1,23 @@
 package controller;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Parser {
 
-	public Parser(){
+	int amountOfPlayers = 0;
+	boolean roomSupport = false;
+	int maxRoomDimensionX = 0;
+	int maxRoomDimensionY = 0;
+	int maxRoomDimensionZ = 0;
+	int maxLengthToWin = 0;
+	boolean chatSupport = false;
+	String playerName = null;
+	boolean autoRefresh = false;
+	int roomID = 0;
+	int playerID = 0;
+	
+	public Parser() {
 
 	}
 
@@ -15,19 +28,10 @@ public class Parser {
 		if (lineScanner.hasNext()) {
 			String word = lineScanner.next();
 
-			int amountOfPlayers = 0;
-			boolean roomSupport = false;
-			int maxRoomDimensionX = 0;
-			int maxRoomDimensionY = 0;
-			int maxRoomDimensionZ = 0;
-			int maxLengthToWin = 0;
-			boolean chatSupport = false;
-			String playerName = null;
-			boolean autoRefresh = false;
-			int roomID = 0;
 			switch (word) {
 
 			case "serverCapabilities":
+				//parse string and store the variables
 				amountOfPlayers = Integer.parseInt(lineScanner.next());
 				if (lineScanner.next().equals("1")) {
 					roomSupport = true;
@@ -46,10 +50,13 @@ public class Parser {
 				} catch (InvalidPipedDataException e) {
 					//TODO error 7
 				}
+				
 				//TODO process client based on these capabilities
+				
 				break;
 
 			case "sendCapabilities":
+				//parse string and store the variables
 				amountOfPlayers = Integer.parseInt(lineScanner.next());
 				playerName = lineScanner.next();
 				if (lineScanner.next().equals("1")) {
@@ -72,11 +79,15 @@ public class Parser {
 				} catch (InvalidPipedDataException e) {
 					//TODO error 7
 				}
+				
 				//TODO process this to capabilities
+				
 				break;
 
 			case "sendListRooms":
+				//parse string and store the variables
 				String room = null;
+				ArrayList<int[]> lobbyData = new ArrayList<int[]>();
 				while (lineScanner.hasNext()) {
 					room = lineScanner.next();
 					int[] roomData = new int[6];
@@ -87,15 +98,20 @@ public class Parser {
 							roomData[i] = Integer.parseInt(roomScanner.next());
 						}
 						if (roomScanner.hasNext()) {
+							roomScanner.close();
 							throw new InvalidPipedDataException(msg, room);
 						}
+						roomScanner.close();
 					} catch (NumberFormatException e) {
 						//TODO send error 7
 					} catch (InvalidPipedDataException e) {
 						//TODO send error 7
 					}
+					lobbyData.add(roomData);
 				}
+				
 				//TODO process this to rooms
+				
 				break;
 				
 				
@@ -104,8 +120,8 @@ public class Parser {
 				break;
 			
 			case "joinRoom":
+				//parse string and store the variables
 				roomID = Integer.parseInt(lineScanner.next());
-				//TODO let server assign socket to specefied room
 				try {
 					if (lineScanner.hasNext()) {
 						throw new InvalidPipedDataException(msg, lineScanner.next());
@@ -113,7 +129,35 @@ public class Parser {
 				} catch (InvalidPipedDataException e) {
 					//TODO error 7
 				}
+				
+				//TODO let server assign socket to specefied room
+				
 				break;	
+				
+			case "error":
+				System.out.println("recieved error " + lineScanner.next());
+				try {
+					if (lineScanner.hasNext()) {
+						throw new InvalidPipedDataException(msg, lineScanner.next());
+					}
+				} catch (InvalidPipedDataException e) {
+					//TODO error 7
+				}
+				break;
+				
+			case "assignID":
+				playerID = Integer.parseInt(lineScanner.next());
+				try {
+					if (lineScanner.hasNext()) {
+						throw new InvalidPipedDataException(msg, lineScanner.next());
+					}
+				} catch (InvalidPipedDataException e) {
+					//TODO error 7
+				}
+				
+				//TODO create player with ID, Name stc.
+				
+				break;
 				
 			default:
 			}

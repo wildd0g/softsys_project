75ba2b6bb02ce39117f95d346b93eb0ba4e2b480
Merel <Merel@LAPTOP-4OEJA8EL>
2017-01-26 11:05:21 +0100

    11:05 added functionality to Player, documentation of Server

--- softsys_project/src/controller/Player.java
+++ softsys_project/src/controller/Player.java
@@ -1,6 +1,11 @@
 package controller;
 
 import java.net.Socket;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class Player {
 	
@@ -8,11 +13,23 @@ public class Player {
 	private String playerName;
 	private Socket playerSocket;
 	private Game currentGame;
+	private BufferedReader input;
+	private BufferedWriter output;
+	
 	//constructor of a player object that can contain information about a player
 	public Player(int id, String name, Socket sock) {
 		this.playerID = id;
 		this.playerName = name;
 		this.playerSocket = sock;
+		
+		try {
+			this.input = new BufferedReader(
+					new InputStreamReader(playerSocket.getInputStream()));
+			this.output = new BufferedWriter(
+					new OutputStreamWriter(playerSocket.getOutputStream()));
+		} catch (IOException io1) {
+			io1.getMessage();
+		}
 	}
 	
 	
@@ -38,6 +55,14 @@ public class Player {
 	
 	public int getID() {
 		return playerID;
+	}
+	
+	public BufferedReader getIn() {
+		return input;
+	}
+	
+	public BufferedWriter getOut() {
+		return output;
 	}
 
 	public Game getGame() {
--- softsys_project/src/controller/Server.java
+++ softsys_project/src/controller/Server.java
@@ -10,12 +10,12 @@ import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class Server {
 	
 	private static List<Player> nonPlaying = new ArrayList<>();
 	private static Map<Game, Player> games = new HashMap<Game, Player>();
 
+	private static boolean receiving = true;
 	private static boolean active = true;
 	private static Socket registrationSocket = null;
 	private static ServerSocket serverSocket = null;
@@ -25,7 +25,7 @@ public class Server {
 	//closes all games
 	//disconnects all Clients
 	public void serverShutDown() {
-		
+		receiving  = false;
 	}
 	
 	public static void main(String[] args) {
@@ -34,6 +34,7 @@ public class Server {
 			System.exit(0);
 		}
 		
+		//assign server name and initialise port
 		String name = args[0];
 		int port = 0;
 		
@@ -46,6 +47,7 @@ public class Server {
 			System.exit(0);
 		}
 		
+		//assign server socket at listening port
 		try {
 			serverSocket = new ServerSocket(port);
 		} catch (IOException io) {
@@ -53,20 +55,29 @@ public class Server {
 			System.out.println(io.getMessage());
 		}
 		
-		while (active) {
+		//While loop to accept new clients wanting to link to the server
+		while (receiving) {
+			
 			try {
+				
+				//socket reference is loaded with new attached socket
 				registrationSocket = serverSocket.accept();
-				nonPlaying.add(new Player(playerCounter, "player" + playerCounter, registrationSocket));
+				
+				//player object is created with this as reference. 
+				//It is added to the list of players not yet in a game and is activated
+				Player newPlayer = 
+						new Player(playerCounter, "player" + playerCounter, registrationSocket);
+				nonPlaying.add(newPlayer);
 				nonPlaying.get(nonPlaying.size() - 1).activate();
+				
+				//Testcode
+				System.out.println(newPlayer.getID());
+				System.out.println(newPlayer.getName());
+				
 			} catch (IOException io1) {
 				//TODO handle exception properly
 				io1.getMessage();
 			}
-			
-			
-			
-			//method to set up communication before getting to game
-			//create player, assign id, add to list
 		}
 		
 	}

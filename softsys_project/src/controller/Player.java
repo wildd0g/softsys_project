393ac57ef3b8f7d2be06c22e153cb1fc2fa8810a
Merel <Merel@LAPTOP-4OEJA8EL>
2017-01-31 14:03:00 +0100

    14:02 capabilities implementation player

--- softsys_project/src/controller/Player.java
+++ softsys_project/src/controller/Player.java
@@ -20,6 +20,15 @@ public class Player {
 	public Sender send;
 	public Receiver receiver;
 	
+	//Capabilities of player
+	private int maxPlayers;
+	private int maxX;
+	private int maxY;
+	private int maxZ;
+	private int maxWin;
+	private boolean chat;
+	private boolean refresh;
+	
 	//constructor of a player object that can contain information about a player
 	public Player(int id, String name, Socket sock) {
 		this.playerID = id;
@@ -27,23 +36,46 @@ public class Player {
 		this.playerSocket = sock;
 		this.send = new Sender(playerSocket);
 		this.receiver = new Receiver(playerSocket);
-	}
-	
-	
-	public void setDefaultCapabilities() {
-		setCapabilities();
-	}
-	
-	public void setCapabilities() {
 		
+		//set default capabilities
+		maxPlayers = 2;
+		maxX = 4;
+		maxY = 4;
+		maxZ = 4;
+		maxWin = 4;
+		chat = false;
+		refresh = false;
 	}
 	
+	public void setCapabilities(int amountOfPlayers, 
+			String name, 
+			int maxDimX,
+			int maxDimY,
+			int maxDimZ,
+			int maxLength,
+			boolean chatSup,
+			boolean refreshOn) {
+		
+		this.playerName = name;
+		this.maxPlayers = amountOfPlayers;
+		this.maxX = maxDimX;
+		this.maxY = maxDimY;
+		this.maxZ = maxDimZ;
+		this.maxWin = maxLength;
+		this.chat = chatSup;
+		this.refresh = refreshOn;
+	}
+		
 	public void setGame(Game game) {
 		this.currentGame = game;
 	}
 	
 	public Socket getSocket() {
 		return playerSocket;
+	}
+	
+	public String getCapabilities() {
+		//TODO finish
 	}
 	
 	public String getName() {

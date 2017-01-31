e24229f3c755817f4fd962d11f618aa523292a5e
wildd0g <mpeppelman2008@hotmail.com>
2017-01-31 15:47:20 +0100

    first implementation of smart AI

--- softsys_project/src/aistrategies/SmartStrategy.java
+++ softsys_project/src/aistrategies/SmartStrategy.java
@@ -2,7 +2,9 @@ package aistrategies;
 
 import java.util.Set;
 
+import controller.Client;
 import model.Board;
+import model.InvalidFieldException;
 import model.Mark;
 
 public class SmartStrategy extends Strategy {
@@ -14,44 +16,66 @@ public class SmartStrategy extends Strategy {
 	}
 
 	@Override
-	public int determineMove(Board board, Mark m) {
+	public void determineMove(Board board, Mark m) {
 		// TODO Auto-generated method stub
 		//get a default random picked move 
 		Set<Integer[]> valid = validMoves(board);
 
 		Object[] validArray = valid.toArray();
-		int input = (int)Math.floor(Math.random() * valid.size());
-		int move = (int)validArray[input];
+		int input = (int) Math.floor(Math.random() * valid.size());
+		Integer[] move = (Integer[]) validArray[input];
 
 		//check if there is a winning move (either for XX or for OO)
-		int winMoveSelf = winningMove(board.deepCopy(), m, validArray);
-		int winMoveOther = winningMove(board.deepCopy(), m.other(), validArray);
+		Integer[] winMoveSelf = winningMove(board.deepCopy(), m, validArray);
+		Integer[] winMoveOther = winningMove(board.deepCopy(), m.cycleReverse(), validArray);
 		
 		//if it exists, chose that move for the computer, or to block it.
-		if (valid.contains(4)){
-			move = 4;
-		}else if (winMoveSelf != -1){
+		Integer[] base = {-1, -1};
+		if (winMoveSelf.equals(base)) {
 			move = winMoveSelf;
-		}else if (winMoveOther != -1){
+		} else if (winMoveOther.equals(base)) {
 			move = winMoveOther;
 		}
 
-		return move;
+		Client.makeMove(move[0], move[1]);
 	}
 
-	int winningMove(Board b, Mark m, Object[] validArray){
-		int move = -1;
-		for (int i = 0; i < validArray.length; i++){
+	private Integer[] winningMove(Board b, Mark m, Object[] validArray) {
+		Integer[] move = {-1, -1};
+		for (int i = 0; i < validArray.length; i++) {
 			Board testB = b.deepCopy();
-			testB.setField((int)validArray[i], m);
-			if (testB.hasWinner()){
-				move = (int)validArray[i];
+			
+			Integer[] moveSet = (Integer[]) validArray[i];
+			testMove(testB, moveSet[0], moveSet[1], m);
+			if (testB.hasWinner()) {
+				move = moveSet;
 				break;
 			}
 		}
+		return move;
 		
-		
-		return move;	
+			
 	}
 
+	private void testMove(Board board, int x, int y, Mark m) {
+		int testZ = 0;
+		boolean empty = false;
+		while (board.isField(x, y, testZ) && !empty) {
+			try {
+				empty = board.isEmptyField(x, y, testZ);
+			} catch (InvalidFieldException e) {
+				empty = true;
+			}
+			if (!empty) {
+				testZ++;
+			}
+		} //returned z if z is not a field or is empty
+		try {
+			board.setField(x, y, testZ, m);
+		} catch (InvalidFieldException e) {
+			e.getMessage();
+		}
+	}
+	
+	
 }
